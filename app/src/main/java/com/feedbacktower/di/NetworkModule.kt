c6bb68f3e46930ae345a10e9d7d6c6b78a82243f
Sanket N <dev.naiksan@gmail.com>
2020-08-20 01:41:15 +0530

    Changed Places api key

--- app/src/main/java/com/feedbacktower/di/NetworkModule.kt
+++ app/src/main/java/com/feedbacktower/di/NetworkModule.kt
@@ -56,7 +56,7 @@ class NetworkModule {
                 return@ani response
             }
             addInterceptor(HttpLoggingInterceptor().apply {
-                level  = HttpLoggingInterceptor.Level.BODY
+                level  = HttpLoggingInterceptor.Level.NONE
             })
         }
         return clientBuilder.build()
